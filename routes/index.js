fe4d24bd2711b927e9fbbb3b5966833d8a503b4d
Rohit Sharma <rohit.sharma1@naukri.com>
2016-08-27 15:45:06 +0530

    ads

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var app = express();
 var VALIDATION_TOKEN = 'quiet-mountain-52358-digiHealth';
+var PAGE_ACCESS_TOKEN = 'EAAE31e3twm0BAPgJxPtB3RcCNam4edtakjUVWGFsfQ4DdmFKl47RtRTnQRRl55t1BBFjttZAg77KlMZB9REsLNvQ4Ahr3Oav8sK5mD5rQlAkKLUZCZB7rjZCdkEQNFm6qrVALt0ZBlxLz4SVZA8GQi8eHdJUxN8UohDVYZC2gTeThAZDZD';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
