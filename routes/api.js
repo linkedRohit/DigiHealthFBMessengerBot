682b862094d1ed54269592876ee011f3d859015e
Rohit Sharma <rohit.sharma1@naukri.com>
2016-08-28 12:25:26 +0530

    ad privacy link

--- routes/api.js
+++ routes/api.js
@@ -10,4 +10,8 @@ router.post('/try', function(req, res, next) {
   res.send('Welcome to our digiHealth Bot API.');
 });
 
+router.get('/privacy', function(req, res, next) {
+  res.send('Your data is secure with us and trust us we are not sharing it with anybody.');
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -300,4 +300,36 @@ function callThreadsManagementAPI(greetingsData) {
 function manageThreads() {
 
 }
+
+function handle_database(req,res) {
+    
+    pool.getConnection(function(err,connection){
+        if (err) {
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }   
+
+        console.log('connected as id ' + connection.threadId);
+        
+        connection.query("select * from user",function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+            }           
+        });
+
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;     
+        });
+  });
+}
+
+app.get("/",function(req,res){-
+        handle_database(req,res);
+});
+
+app.listen(3000);
+
+
 module.exports = router;
